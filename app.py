907052f26843bed0254c44bc76d032262095ca23
EC2 Default User <ec2-user@ip-172-31-46-207.eu-north-1.compute.internal>
2025-12-30 19:22:10 +0000

    Add HTML homepage and improve UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import socket
 import datetime
 import os
@@ -38,8 +38,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello DevOps! CI/CD project is running 🚀"
+def home():
+    return render_template("index.html")
+  
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
